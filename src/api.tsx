955dfc0cff08e5d5a1a98a2e780e9daa2a8b0baf
Kate Moser <komoser@gmail.com>
2024-06-10 16:59:38 -0700

    adding mashup to api

--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,5 @@
-const MARKOV_API_BASE_URL = "http://127.0.0.1:5001"
-import {ISeed} from "./interfaces"
+const MARKOV_API_BASE_URL = "http://127.0.0.1:5001";
+import { ISeed } from "./interfaces";
 
 // moved to interfaces
 // interface ISeed {
@@ -14,10 +14,24 @@ import {ISeed} from "./interfaces"
 
 class MashupApi {
 
-    static async getSeeds() : Promise<ISeed[]>{
+    static async getSeeds(): Promise<ISeed[]> {
         const response = await fetch(`${MARKOV_API_BASE_URL}/seeds`);
-        const seedData :{seeds: ISeed[]} = await response.json();
+        const seedData: { seeds: ISeed[]; } = await response.json();
         return seedData.seeds;
+    }
+
+    static async mashUp(ids: number[]): Promise<string> {
+        const response = await fetch(
+            `${MARKOV_API_BASE_URL}/mashups`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ seed_ids: ids })
+            });
+        const data = await response.json()
+        return data.mashup;
     }
 }
 
